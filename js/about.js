06c6b76f53898ac68a5bc0e76349a6dd508bb9ef
Narinder-Singh00 <narinderkhuttan1005@gmail.com>
2026-02-19 13:56:30 +0530

    update

--- js/about.js
+++ js/about.js
@@ -1,10 +1,30 @@
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded",()=>{
 
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-
-hamburger.onclick = ()=>{
-navLinks.classList.toggle("active");
+const nav=document.querySelector(".nav-links");
+document.querySelector(".hamburger").onclick=()=>{
+nav.classList.toggle("active");
 };
+
+const slides=document.querySelectorAll(".about-slide");
+
+let index=0;
+
+function showSlide(){
+
+slides.forEach(slide=>slide.classList.remove("active"));
+
+slides[index].classList.add("active");
+
+index++;
+
+if(index>=slides.length){
+index=0;
+}
+
+}
+
+showSlide();
+
+setInterval(showSlide,3000);
 
 });
